284b6fbb1a97558187d05c9b945c51188d52a7b1
Muzakie-ID <76565734+Muzakie-ID@users.noreply.github.com>
2023-10-09 22:01:00 +0700

    Update Technologies.js

--- src/Pages/Technologies.js
+++ src/Pages/Technologies.js
@@ -31,12 +31,12 @@ function Technologies() {
       <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6">
         <img src={html} title="html" alt="" />
         <img src={css} title="CSS" alt="" />
-        <img src={js} title="JavaScript" alt="" />
-        <img src={react} title="React" alt="" />
-        <img src={redux} title="Redux" alt="" />
-        <img src={tailwind} title="Tailwind CSS" alt="" />
-        <img src={bootstrap} title="Bootstrap" alt="" />
-        <img src={sass} title="SASS" alt="" />
+//        <img src={js} title="JavaScript" alt="" />
+//        <img src={react} title="React" alt="" />
+//        <img src={redux} title="Redux" alt="" />
+//        <img src={tailwind} title="Tailwind CSS" alt="" />
+//        <img src={bootstrap} title="Bootstrap" alt="" />
+//        <img src={sass} title="SASS" alt="" />
       </section>
       <section>
         <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
@@ -45,11 +45,11 @@ function Technologies() {
       </section>
       <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6">
         <img src={vscode} title="Visual Studio Code" alt="" />
-        <img src={git} title="Git" alt="Git" />
+//        <img src={git} title="Git" alt="Git" />
         <img src={github} title="Github" alt="Github" />
-        <img src={figma} title="Figma" alt="Figma" />
-        <img src={npm} title="NPM" alt="NPM" />
-        <img src={postman} title="Postman" alt="Postman" />
+//        <img src={figma} title="Figma" alt="Figma" />
+//        <img src={npm} title="NPM" alt="NPM" />
+//        <img src={postman} title="Postman" alt="Postman" />
       </section>
     </main>
   );
